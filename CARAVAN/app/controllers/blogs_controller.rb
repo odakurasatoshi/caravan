d32564c4736a97ba04c0783e53b1aa4d1710e9e0
vagrant <vagrant@localhost.localdomain>
2020-05-18 09:35:54 +0000

    [Finish]4-4

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -1,13 +1,11 @@
 class BlogsController < ApplicationController
   def show
   end
-  
+
   def index
   	# 記事を全件取得
   	@blogs =Blog.all
   end
-
-
 
   def new
   	@blog = Blog.new
@@ -19,11 +17,14 @@ class BlogsController < ApplicationController
   	  redirect_to blogs_path
   end
 
+  def edit
+  end
+
   private
   def blog_params
   	params.require(:blog).permit(:title, :category, :body)
   end
 
-  def edit
-  end
+
+
 end
